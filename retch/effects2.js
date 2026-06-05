9649e5ae7267a532e4a06415b17350395ab7bea6
Rainy1337 <revrainyxyz@gmail.com>
2026-06-05 23:10:05 +0800

    Add files via upload

---
+++ retch/effects2.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const imgSrc = "https://lanyard.cnrad.dev/api/863769976687427594?bg=863769976687427594&animated=false&showDisplayName=true&hideSpotify=true";
+    const imgElement = document.getElementById('rpc');
+    imgElement.src = imgSrc;
+});
